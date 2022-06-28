cda4feda8a524bcb4145a30a32830451912f01d7
CarinaAJ <carinastenger@yahoo.com>
2022-06-28 15:43:50 +0000

    Connect buttons with DOM and display choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,33 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+let userPick;
 
 let computerPickDisplay = document.getElementById("computer-pick");
 
 // computer pick function
 
-function createComputerChoice(){
+function createComputerChoice() {
     const rpsOptions = ['rock', 'paper', 'scissors'];
-    const randomChoice = Math.floor(Math.random()*3);
+    const randomChoice = Math.floor(Math.random() * 3);
     return rpsOptions[randomChoice];
 }
 
-computerPickDisplay.innerHTML = createComputerChoice();
+computerPickDisplay.innerHTML = `Computer chose ${createComputerChoice()}`;
+
+// user event listener
+
+rock.addEventListener("click", function () {
+    let userPick = document.getElementById("user-pick");
+    userPick.innerHTML = "You chose Rock";
+});
+
+paper.addEventListener("click", function () {
+    let userPick = document.getElementById("user-pick");
+    userPick.innerHTML = "You chose Paper";
+});
+
+scissors.addEventListener("click", function () {
+    let userPick = document.getElementById("user-pick");
+    userPick.innerHTML = "You chose Scissors";
+});
