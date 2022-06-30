fa7776a8ad859978ebbd9508d7faf6821246ca16
CarinaAJ <carinastenger@yahoo.com>
2022-06-30 18:23:06 +0000

    Create function to run game and compare results

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,33 +2,53 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
-let userPick;
+const rpsOptions = ['Rock', 'Paper', 'Scissors'];
+const result = document.getElementById("result");
 
+// replace ABOVE with just choices to create a more generic function?
+
+// computerPickDisplay needed!
 let computerPickDisplay = document.getElementById("computer-pick");
+const choices = document.querySelectorAll("rps-buttons");
+
+
 
 // computer pick function
 
 function createComputerChoice() {
-    const rpsOptions = ['rock', 'paper', 'scissors'];
     const randomChoice = Math.floor(Math.random() * 3);
     return rpsOptions[randomChoice];
 }
 
-computerPickDisplay.innerHTML = `Computer chose ${createComputerChoice()}`;
 
-// user event listener
+// user event listener - must be an easier way to do this
+function game(userChoice) {
+    let userPick = document.getElementById("user-pick");
+    userPick.innerHTML = `chose ${userChoice}`;
+    let computerChoice = createComputerChoice();
+    computerPickDisplay.innerHTML = `Computer chose ${computerChoice}`;
+
+    displayResult(userChoice, computerChoice);
+
+}
+
+function displayResult(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        result.innerHTML = "It's a tie";
+    }
+    
+}
+
+
 
 rock.addEventListener("click", function () {
-    let userPick = document.getElementById("user-pick");
-    userPick.innerHTML = "You chose Rock";
+    game("Rock");
 });
 
 paper.addEventListener("click", function () {
-    let userPick = document.getElementById("user-pick");
-    userPick.innerHTML = "You chose Paper";
+    game("Paper");
 });
 
 scissors.addEventListener("click", function () {
-    let userPick = document.getElementById("user-pick");
-    userPick.innerHTML = "You chose Scissors";
+    game("Scissors");
 });
