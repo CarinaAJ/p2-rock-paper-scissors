39a7d444bdd5afc5b795fd53f42d8528a58f4161
CarinaAJ <carinastenger@yahoo.com>
2022-06-30 18:27:12 +0000

    Tidy up comments and remove whitespaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,32 @@ const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const rpsOptions = ['Rock', 'Paper', 'Scissors'];
 const result = document.getElementById("result");
-
-// replace ABOVE with just choices to create a more generic function?
-
-// computerPickDisplay needed!
 let computerPickDisplay = document.getElementById("computer-pick");
 const choices = document.querySelectorAll("rps-buttons");
 
-
-
-// computer pick function
+// let computer pick at random
 
 function createComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     return rpsOptions[randomChoice];
 }
 
-
-// user event listener - must be an easier way to do this
+// run game function
 function game(userChoice) {
     let userPick = document.getElementById("user-pick");
     userPick.innerHTML = `chose ${userChoice}`;
     let computerChoice = createComputerChoice();
     computerPickDisplay.innerHTML = `Computer chose ${computerChoice}`;
-
     displayResult(userChoice, computerChoice);
-
 }
 
 function displayResult(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         result.innerHTML = "It's a tie";
     }
-    
 }
 
-
-
+// event listeners for each button
 rock.addEventListener("click", function () {
     game("Rock");
 });
